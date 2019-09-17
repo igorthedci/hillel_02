6b6789ed4c7fe1119dd43af7a34023517ab427de
igorthedci <foumart@ukr.net>
2019-09-17 22:40:42 +0300

    task 02: NEW branch

--- src/main/java/task_02.java
+++ src/main/java/task_02.java
@@ -3,6 +3,6 @@ public class task_02 {
     // 3) Write any program you wish(up to you) and push it to your repo to a new branch.
 
     public static void main(String[] args) {
-        System.out.println("hello master branch!");
+        System.out.println("hello NEW branch!");
     }
 }
