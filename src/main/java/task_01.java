54b97484ea8fe3e141db4de7ea4b1e87f778a63c
igorthedci <foumart@ukr.net>
2019-09-17 22:37:25 +0300

    task 02: master branch

--- src/main/java/task_01.java
+++ src/main/java/task_01.java
@@ -1,4 +1,7 @@
 public class task_01 {
+
+    // 2) Create a new Maven project "Hello world" and add it to your Github repo.
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
---
+++ src/main/java/task_02.java
@@ -0,0 +1,8 @@
+public class task_02 {
+
+    // 3) Write any program you wish(up to you) and push it to your repo to a new branch.
+
+    public static void main(String[] args) {
+        System.out.println("hello master branch!");
+    }
+}
